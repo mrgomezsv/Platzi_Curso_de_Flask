3417e80ec2cb206017efeb6cb6df90747a7f808c
Mario Roberto <mrgomez.dev@gmail.com>
2024-02-08 15:45:11 -0600

    Bootstrap integration to Flask

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app) #Inicializamos bootstrap
 
 todos = ['Comprar Café', 'Enviar Solicitudes de Comrpa', 'Entregar video al productor']
 
