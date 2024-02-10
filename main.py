c2259a541e6843e0e184b0bfbe209f2a549471a5
Mario Roberto <mrgomez.dev@gmail.com>
2024-02-09 18:43:07 -0600

    App Factory for Project

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+# from flask_bootstrap import Bootstrap
+# from flask_wtf import FlaskForm
+# from wtforms.fields import StringField, PasswordField, SubmitField
+# from wtforms.validators import DataRequired
 import unittest
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app) #Inicializamos bootstrap
+from app import create_app
+from app.forms import LoginForm
 
-app.config['SECRET_KEY'] = 'SUPER SECRETO' #Estos nos ayuda a generar una sesion en flask
+app = create_app()
+# app = Flask(__name__)
+# bootstrap = Bootstrap(app) #Inicializamos bootstrap
+
+# app.config['SECRET_KEY'] = 'SUPER SECRET' #Estos nos ayuda a generar una sesion en flask
 
 todos = ['Comprar Café', 'Enviar Solicitudes de Comrpa', 'Entregar video al productor']
 
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de Usuario', validators=())
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
+# class LoginForm(FlaskForm):
+#     username = StringField('Nombre de Usuario', validators=())
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     submit = SubmitField('Enviar')
     
 @app.cli.command()
 def test():
